f19795613a4397393b3d019e59d5bcb8c973d829
thiago <thiagolocatellicdeoliveira2gmail.com>
2017-08-22 10:17:12 -0300

    admin

--- app_syfy/admin.py
+++ app_syfy/admin.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from app_syfy.models.usuario import Usuario
+from app_syfy.models.autor import Autor
+from app_syfy.models.classificacao import Classificacao
+from app_syfy.models.genero import Genero
+from app_syfy.models.video import Video
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Video)
+admin.site.register(Genero)
+admin.site.register(Autor)
+admin.site.register(Classificacao)
