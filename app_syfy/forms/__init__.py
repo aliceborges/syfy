d34a7ecdc3db53af9f50da41df6042cb1d7a26b2
marilia_ribeiro <marilia.ifc@gmail.com>
2017-08-22 10:06:26 -0300

    criando diretórios views e forms

---
+++ app_syfy/forms/__init__.py
@@ -0,0 +1,3 @@
+from app_syfy.forms.autor import *
+from app_syfy.forms.genero import *
+from app_syfy.forms.classificacao import *
--- app_syfy/views/__init__.py
+++ app_syfy/views/__init__.py
@@ -1,1 +1,3 @@
-from app_syfy.views.autor import *
+# from app_syfy.views.autor import *
+# from app_syfy.views.genero import *
+# from app_syfy.views.classificacao import *
